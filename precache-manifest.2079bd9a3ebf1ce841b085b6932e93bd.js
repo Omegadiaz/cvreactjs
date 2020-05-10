24b33e6e4c26bad9c52f9f03b1d8effd37c3eff9
Omegadiaz <omsystem.gabriel@gmail.com>
2020-05-10 01:55:41 +0100

    Updates

---
+++ precache-manifest.2079bd9a3ebf1ce841b085b6932e93bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdb09544eb681c1756b5c32e7e91f8b",
+    "url": "/cvreactjs/index.html"
+  },
+  {
+    "revision": "ff7696890f847cfb832a",
+    "url": "/cvreactjs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d0c6c900d68f56bd4e9f",
+    "url": "/cvreactjs/static/js/2.71d637c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cvreactjs/static/js/2.71d637c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7696890f847cfb832a",
+    "url": "/cvreactjs/static/js/main.7054b2da.chunk.js"
+  },
+  {
+    "revision": "1109315879b070c4582f",
+    "url": "/cvreactjs/static/js/runtime-main.247f765c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cvreactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
